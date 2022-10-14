88f860684e13e8ca925551760ba9c52e20abb48c
amptxs <72046996+amptxs@users.noreply.github.com>
2022-10-14 11:51:49 +0500

    Hover + плоттер красивый

--- LinearPlotter.py
+++ LinearPlotter.py
@@ -1,7 +1,9 @@
 import re
 from numpy import arange, linspace, pi, sin
+import numpy as np
 
-from bokeh.models import LinearAxis, Range1d, Title
+from bokeh.models import LinearAxis, Range1d, Title, NumeralTickFormatter, FactorRange, SingleIntervalTicker
+from bokeh.models.ranges import DataRange1d
 from bokeh.plotting import figure, output_notebook, show
 
 
@@ -19,11 +21,14 @@ class LinearPlotter:
         main_plot = figure(
             title=Title(text="Динамика технологических показателей скв. " + self.name, align='center'),
             sizing_mode="stretch_width",
+            height=750,
             x_axis_label="Годы",
             x_range=x_axis,
-            height=500
+            # x_range=FactorRange(*x_axis),
+            y_range=None
         )
         # pass
+        main_plot.xaxis.major_label_orientation = "vertical"
         main_plot = self.add_layouts(main_plot)
         return main_plot
 
@@ -32,30 +37,67 @@ class LinearPlotter:
         plot.extra_y_ranges = {
             "oil_debit": Range1d(start=0, end=150),
             'liquid_debit': Range1d(start=0, end=100),
-            'water_cut': Range1d(start=0, end=100),
+            'water_cut': Range1d(start=0, end=1),
             'pressure': Range1d(start=0, end=500)
         }
 
+        plot = self.create_line(self.data, 'Дата', 'Дебит_нефти,т/сут', plot, 'brown', 'oil_debit')
+        plot = self.create_line(self.data, 'Дата', 'Дебит_жидкости,м3/сут', plot, 'green', 'liquid_debit')
+        plot = self.create_line(self.data, 'Дата', 'Обводненность вес.,%', plot, 'blue', 'water_cut')
+        # plot = self.create_line(self.data, 'Дата', 'Давление на ВДП_пластовое', plot, 'green', 'pressure')
+
+        plot = self.create_circle(self.data, 'Дата', 'Давление на ВНК_пластовое', plot, 'green', 'pressure')
+
+        plot = self.create_bar(self.data, 'Дата', 'Давление на ВНК_пластовое', plot, 'green', 'pressure')
+
         plot.add_layout(self.create_y_axis("water_cut", "Обводненность"), 'left')
-        plot.add_layout(self.create_y_axis("liquid_debit", "Дебит, м3/сут"), 'left')
         plot.add_layout(self.create_y_axis("oil_debit", "Дебит, т/сут"), 'left')
+        plot.add_layout(self.create_y_axis("liquid_debit", "Дебит, м3/сут"), 'left')
         plot.add_layout(self.create_y_axis("pressure", "Давление, ат"), 'right')
-
-        plot = self.create_line(self.data, 'Дата', 'Дебит_нефти,т/сут', plot, 'brown')
-        plot = self.create_line(self.data, 'Дата', 'Дебит_жидкости,м3/сут', plot, 'green')
-        plot = self.create_line(self.data, 'Дата', 'Обводненность вес.,%', plot, 'blue')
-        plot = self.create_line(self.data, 'Дата', 'Давление на ВДП_пластовое', plot, 'green')
-
-        plot = self.create_circle(self.data, 'Дата', 'Давление на ВНК_пластовое', plot, 'green')
-
-        plot = self.create_bar(self.data, 'Дата', 'Давление на ВНК_пластовое', plot, 'green')
 
         return plot
 
     def create_y_axis(self, name, label):
-        return LinearAxis(y_range_name=name, axis_label=label, axis_label_text_font_style='normal')
+        if (label == 'Обводненность'):
+            return LinearAxis(y_range_name=name, axis_label=label, axis_label_text_font_style='normal',
+                              formatter=NumeralTickFormatter(format='0 %'))
+        elif (label == 'Дебит, т/сут'):
+            return LinearAxis(y_range_name=name, axis_label=label, axis_label_text_font_style='normal',
+                              ticker=SingleIntervalTicker(interval=15))
+        else:
+            return LinearAxis(y_range_name=name, axis_label=label, axis_label_text_font_style='normal')
 
-    def create_line(self, data, x_label, y_label, plot, color="black"):
+    def create_line(self, data, x_label, y_label, plot, color="black", y_range_name='default'):
+        x = []
+        y = []
+
+        # y_range_name = 'default'
+
+        for index, row in data.iterrows():
+
+            x_value = row[x_label]
+            if (x_value[-4] == '.'):
+                x_value = x_value + "0"
+
+            x.append(x_value)
+
+            if y_range_name == 'water_cut':
+                y.append(row[y_label] / 100)
+            else:
+                y.append(row[y_label])
+
+            # Дебит_нефти,т/сут
+
+        if y_range_name != '':
+            line = plot.line(x, y, legend_label=y_label, color=color, x_range_name='default', y_range_name=y_range_name)
+        else:
+            line = plot.line(x, y, legend_label=y_label, color=color)
+
+        # plot.renderers = plot.renderers.append(line)
+
+        return plot
+
+    def create_circle(self, data, x_label, y_label, plot, color='black', y_range_name='default'):
         x = []
         y = []
 
@@ -66,14 +108,14 @@ class LinearPlotter:
                 x_value = x_value + "0"
 
             x.append(x_value)
-            y.append(500 + row[y_label])
+            y.append(row[y_label])
 
             # Дебит_нефти,т/сут
 
-        plot.line(x, y, legend_label=y_label, color=color)
+        plot.circle(x, y, legend_label=y_label, color=color, y_range_name=y_range_name)
         return plot
 
-    def create_circle(self, data, x_label, y_label, plot, color='black'):
+    def create_bar(self, data, x_label, y_label, plot, color='black', y_range_name='default'):
         x = []
         y = []
 
@@ -88,27 +130,8 @@ class LinearPlotter:
             # y.append(100 + row[y_label])
 
             # Дебит_нефти,т/сут
-
-        plot.circle(x, y, legend_label=y_label, color=color)
-        return plot
-
-    def create_bar(self, data, x_label, y_label, plot, color='black'):
-        x = []
-        y = []
-
-        for index, row in data.iterrows():
-
-            x_value = row[x_label]
-            if (x_value[-4] == '.'):
-                x_value = x_value + "0"
-
-            x.append(x_value)
-            y.append(row[y_label])
-            # y.append(100 + row[y_label])
-
-            # Дебит_нефти,т/сут
-
-        plot.rect(x, y, legend_label=y_label, width=0.4, height=y, color=color)
+        y = np.array(y)
+        plot.rect(x, 500, legend_label=y_label, width=0.4, height=-y, color=color, y_range_name=y_range_name)
         return plot
 
     def create_main_x_axis(self, data):
@@ -126,4 +149,4 @@ class LinearPlotter:
                 years_x_axis.append((string_year[-4:], string_year))
 
         return just_months
-        # return years__axis
+        # return years_x_axis
--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import ExcelProcessor as ep
 from math import pi
 from bokeh.io import output_file, show
 from bokeh.plotting import figure, curdoc
-from bokeh.models import CustomJS, RadioButtonGroup, Slider, Button
+from bokeh.models import CustomJS, RadioButtonGroup, Slider, HoverTool
 from bokeh.layouts import column
 
 
 Map = figure(title="Pie map", plot_width=1500, plot_height=700)
-Map.toolbar.active_scroll = Map.select_one(WheelZoomTool)
+#Map.toolbar.active_scroll = Map.select_one(WheelZoomTool)
 
 year = 2016
 defaultRadiusMultiplier = 1.2
@@ -24,10 +24,13 @@ slider = Slider(start=0.1, end=4, step=0.01, value=1.2)
 
 df = ep.normalize(ep.readExcel())
 
+hover = HoverTool(tooltips=[("data", "$name")])
+
 for name in df.keys():
     localPie = df[name].loc[df[name]['Дата'] == year]
     if not localPie.empty:
         if 'X' in localPie:
+            name = localPie['Имя'].values[0]
             X = localPie['X'].values[0]
             Y = localPie['Y'].values[0]
 
@@ -45,15 +48,24 @@ for name in df.keys():
 
             # TODO legend field, hover
             # На point можно цеплять евенты
+            strFormat = f"Дебит жидкости: {debitFluid}, Дебит нефти: {debitPetrol}"
             point = Map.wedge(x=X, y=Y, radius=radius,
                       start_angle=starts, end_angle=ends,
-                      line_color="white", color=pieColors)
+                      line_color="white", color=pieColors, name= strFormat)
+
+            hoverGlyph = Map.circle(x=X, y=Y, radius=radius/2, color="Black", name="Название: " + name)
 
             slider.js_on_change('value',
-                                CustomJS(args=dict(other=point.glyph,size=debitFluid),
+                                CustomJS(args=dict(other=point.glyph, size=debitFluid),
+                                         code="other.radius = this.value * size"))
+            slider.js_on_change('value',
+                                CustomJS(args=dict(other=hoverGlyph.glyph, size=debitFluid/2),
                                          code="other.radius = this.value * size"))
 
 
-dfConcated = ep.sheetsConcat(ep.normalize(ep.readExcel()))
-plotter = LinearPlotter(dfConcated, "15015Г")
+
+
+Map.add_tools(hover)
+dfConcat = ep.sheetsConcat(ep.normalize(ep.readExcel()))
+plotter = LinearPlotter(dfConcat, "15015Г")
 show(column(radio_button_group, slider, Map, plotter.show()))
